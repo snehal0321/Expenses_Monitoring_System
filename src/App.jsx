cf8ac407b0af4e38f6f192ab47e3848ce300514a
snehal0321 <smashelkar6@gmail.com>
2025-10-28 15:07:58 +0530

    fix for react logo

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 import Expenses from './components/Expenses.jsx';
 import './App.css';
+import reactLogo from './assets/react.svg';
 
 function App() { 
 
   return (
     <>
       <div>
-        <img src="/assets/react.svg" alt="React Logo" className='logo' width="100" />
+        <img src={reactLogo} alt="React Logo" className='logo' width="100" />
         <h2>Let's get started!</h2>
         <Expenses />
       </div>
