ddc1664eac3d9eba4cc9327d8953b0a9b8dee484
snehal0321 <smashelkar6@gmail.com>
2025-10-28 14:47:44 +0530

    fix for react logo

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ function App() {
   return (
     <>
       <div>
-        <img src="src/assets/react.svg" alt="React Logo" className='logo' width="100" />
+        <img src="/assets/react.svg" alt="React Logo" className='logo' width="100" />
         <h2>Let's get started!</h2>
         <Expenses />
       </div>
