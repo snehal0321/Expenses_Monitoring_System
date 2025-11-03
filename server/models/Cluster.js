637b3ef9d2b2cd2dba5014736e5bffc08c9e8310
snehal0321 <smashelkar6@gmail.com>
2025-11-03 16:33:15 +0530

    Adding date to Cluster field

--- server/models/Cluster.js
+++ server/models/Cluster.js
@@ -3,6 +3,7 @@ import mongoose from "mongoose";
 const clusterSchema = new mongoose.Schema({
   title: { type: String, required: true },
   balance: { type: Number, required: true },
+  date: { type: Date, default: Date.now },
 });
 
 const Cluster = mongoose.model("Cluster", clusterSchema);
