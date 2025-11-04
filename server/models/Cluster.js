861515ee609175fa671d5645853492f16b335af1
snehal0321 <smashelkar6@gmail.com>
2025-11-04 11:00:17 +0530

    Cluster scheme change 2

--- server/models/Cluster.js
+++ server/models/Cluster.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 const clusterSchema = new mongoose.Schema({
   title: { type: String, required: true },
   balance: { type: Number, required: true },
-  date: { type: Date, default: Date.now },
+  date: { type: Date, required: false },
 });
 
 const Cluster =
